e57e8ad32d38ea26a7545b25c444c6bedcfabeac
Florian <bouclet.florian@hotmail.com>
2024-07-04 23:09:49 +0200

    fix css margin over on mobile

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../components/ContainerService/ContainerService.css'
 import ContainerService from '../components/ContainerService/ContainerService'
 
 const Services = () => {
